201b0a0b06b9c2e1a05273bd94bb531702ee0887
Natalie Arellano <natalieparellano@users.noreply.github.com>
2016-08-28 06:56:29 -0400

    Add bail out condition for 0 tweets
    We can't take log of 0

--- score_service.py
+++ score_service.py
@@ -3,6 +3,9 @@ from math import log
 class ScoreService:
 
   def score( self, val, max_val ):
+    if ( val == 0 ):
+      return 1
+
     max_score   = log( max_val, 10 )
     found_score = log( val, 10 )
     return 1 - ( found_score / max_score )
